6e5959db53088379ceaef17e6e859bec0a8aebee
Your Name <zingorskiy@gmail.com>
2025-04-05 19:18:59 +0300

    max fix

--- task.py
+++
@@ -1,9 +0,0 @@
-class Task:
-    def __init__(self, task_id, title, description, status):
-        self.task_id = task_id
-        self.title = title
-        self.description = description
-        self.status = status
-
-    def __repr__(self):
-        return f"Task({self.task_id}, {self.title}, {self.description}, {self.status})"
